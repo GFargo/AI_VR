162ca1d75c81563d0278038e35735b2d467089b6
Griffen Fargo <griffen@alleyinteractive.com>
2017-04-09 02:02:35 -0400

    updates with new webpack additions

---
+++ app/js/global/index.js
@@ -0,0 +1,4 @@
+
+require('app/scss/main.scss');
+
+require('app/js/ai-vr-public');
---
+++ static/js/admin.bundle.js
@@ -0,0 +1,2 @@
+!function(t){function r(n){if(e[n])return e[n].exports;var o=e[n]={exports:{},id:n,loaded:!1};return t[n].call(o.exports,o,o.exports,r),o.loaded=!0,o.exports}var e={};return r.m=t,r.c=e,r.p="/wp-content/themes/ai-vr/static/",r(0)}([function(t,r){"use strict"}]);
+//# sourceMappingURL=admin.bundle.js.map
